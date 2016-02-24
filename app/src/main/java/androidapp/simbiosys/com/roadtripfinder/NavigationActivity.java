be4686eed10427f5682715e327e81656882cda13
zhuang9 <zhehuang@mtu.edu>
2016-02-23 21:40:12 -0800

    Navigation Activity Update

--- app/src/main/java/androidapp/simbiosys/com/roadtripfinder/NavigationActivity.java
+++ app/src/main/java/androidapp/simbiosys/com/roadtripfinder/NavigationActivity.java
@@ -1,43 +1,70 @@
 package androidapp.simbiosys.com.roadtripfinder;
 
+import android.content.Context;
+import android.util.Log;
+
 import com.google.android.gms.maps.model.LatLng;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by zhehuang on 2/22/16.
  */
 public class NavigationActivity {
 
-    private String getDirectionsUrl(LatLng origin, LatLng dest) {
+    public  NavigationActivity(Context context) {
+        super(context, );
+    }
 
-        // Origin of route
-        String str_origin = "origin=" + origin.latitude + ","
-                + origin.longitude;
-
-        // Destination of route
-        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
-
-        // Sensor enabled
-        String sensor = "sensor=false";
-
-        // Travelling Mode
-        String mode = "mode=driving";
-
-        //waypoints,116.32885,40.036675
-        String waypointLatLng = "waypoints="+"40.036675"+","+"116.32885";
-
-        // Building the parameters to the web service
-        String parameters = str_origin + "&" + str_dest + "&" + sensor + "&"
-                + mode+"&"+waypointLatLng;
-
-        // Output format
-        String output = "json";
-
-        // Building the url to the web service
-        String url = "https://maps.googleapis.com/maps/api/directions/"
-                + output + "?" + parameters;
-        System.out.println("getDerectionsURL--->: " + url);
+    private String getDirectionsUrl(LatLng start, LatLng end) {
+        String url = "https://maps.googleapis.com/maps/api/directions/json?"
+                + "origin=" + start.latitude + "," + start.longitude + "&destination=" + end.latitude + "," + end.longitude;
         return url;
     }
 
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception while downloading url", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        System.out.println("url:" + strUrl + "---->   downloadurl:" + data);
+        return data;
+    }
 
 }
