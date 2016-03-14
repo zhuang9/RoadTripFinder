d7f81746d1e224f7dbdaebaf50793907e7fda82b
zhuang9 <zhehuang@mtu.edu>
2016-03-13 21:27:55 -0700

    Update

---
+++ app/src/main/java/androidapp/simbiosys/com/roadtripfinder/YelpAPIFactory.java
@@ -0,0 +1,70 @@
+package androidapp.simbiosys.com.roadtripfinder;
+
+/**
+ * Created by zhehuang on 3/11/16.
+ */
+import com.squareup.okhttp.OkHttpClient;
+import com.yelp.clientlib.exception.ErrorHandlingInterceptor;
+
+import retrofit.JacksonConverterFactory;
+import retrofit.Retrofit;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+
+
+/**
+ * Util class to create YelpAPI as the stub to use Yelp API. This is the entry point to use this clientlib.
+ * <p>
+ * Example:<br />
+ * YelpAPIFactory apiFactory = new YelpAPIFactory(consumerKey, consumerSecret, token, tokenSecret);<br />
+ * YelpAPI yelpAPI = apiFactory.createAPI();<br />
+ * Business business = yelpAPI.getBusiness(businessId).execute();
+ * </p>
+ */
+public class YelpAPIFactory {
+
+    private static final String YELP_API_BASE_URL = "https://api.yelp.com";
+
+    private OkHttpClient httpClient;
+
+    /**
+     * Construct a new {@code YelpAPIFactory}.
+     *
+     * @param consumerKey    the consumer key.
+     * @param consumerSecret the consumer secret.
+     * @param token          the access token.
+     * @param tokenSecret    the token secret.
+     * @see <a href="https://www.yelp.com/developers/manage_api_keys=>https://www.yelp.com/developers/manage_api_keys</a>
+     */
+    public YelpAPIFactory(String consumerKey, String consumerSecret, String token, String tokenSecret) {
+        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(consumerKey, consumerSecret);
+        consumer.setTokenWithSecret(token, tokenSecret);
+        this.httpClient = new OkHttpClient();
+        this.httpClient.interceptors().add(new SigningInterceptor(consumer));
+        this.httpClient.interceptors().add(new ErrorHandlingInterceptor());
+    }
+
+    /**
+     * Initiate a {@link YelpAPI} instance.
+     *
+     * @return an instance of {@link YelpAPI}.
+     */
+    public YelpAPI createAPI() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getAPIBaseUrl())
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(this.httpClient)
+                .build();
+
+        return retrofit.create(YelpAPI.class);
+    }
+
+    /**
+     * Get the base URL of Yelp APIs.
+     *
+     * @return the base URL of Yelp APIs.
+     */
+    public String getAPIBaseUrl() {
+        return YELP_API_BASE_URL;
+    }
+}
